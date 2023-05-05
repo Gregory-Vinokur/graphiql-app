7359e29c6780196172a84839030e4e0f90983458
Artem Kamyshenkov <artmiller22831@gmail.com>
2023-05-05 22:41:10 +0300

    feat: add registration UI

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -3,50 +3,55 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-
+import Head from 'next/head';
 const LoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   return (
-    <Container maxWidth="xs">
-      <Typography variant="h4" align="center" gutterBottom mt={10}>
-        Login
-      </Typography>
-      <form onSubmit={() => console.log('submit')}>
-        <TextField
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          id="email"
-          label="Email Address"
-          name="email"
-          autoComplete="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <TextField
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          name="password"
-          label="Password"
-          type="password"
-          id="password"
-          autoComplete="current-password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button type="submit" fullWidth variant="contained" color="primary">
-          Sign In
-        </Button>
-        {error && (
-          <Typography variant="body1" align="center" color="error">
-            {error}
-          </Typography>
-        )}
-      </form>
-    </Container>
+    <>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <Container maxWidth="xs">
+        <Typography variant="h4" align="center" gutterBottom mt={10}>
+          Login
+        </Typography>
+        <form onSubmit={() => console.log('submit')}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button type="submit" fullWidth variant="contained" color="primary">
+            Sign In
+          </Button>
+          {error && (
+            <Typography variant="body1" align="center" color="error">
+              {error}
+            </Typography>
+          )}
+        </form>
+      </Container>
+    </>
   );
 };
 
